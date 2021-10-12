fea06e7d82db7a4acf681d27b6289f8d5f6af924
julia-kliot <juliakliot.jk@gmail.com>
2021-10-12 18:35:24 +0300

    commit_new

--- src/test/java/StertSelenium.java
+++ src/test/java/StertSelenium.java
@@ -1,8 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.sql.PseudoColumnUsage;
+import java.util.List;
 
 public class StertSelenium {
     WebDriver wd;
@@ -10,11 +15,15 @@ public class StertSelenium {
     public void preconditions(){
         wd=new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\julia\\Documents\\QA\\QA_Automation\\QA\\Trello_01\\chromedriver.exe");
-                wd.navigate().to("https://trello.com/logged-out");
+                wd.navigate().to("https://trello.com/");
+
     }
 
     @Test
     public void testName(){
+        WebElement a = wd.findElement(By.tagName("a"));
+        List<WebElement> a1 = wd.findElements(By.tagName("a"));
+        System.out.println(a1.size());
 
 
     }
